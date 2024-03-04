278bcca10748ff73027f5c68ff59f6d055a4f791
Yağmur <yagmur@Yagmur-MBP.home>
2024-03-05 02:35:27 +0300

    ilk ders kodu

---
+++ src/layouts/main/rightbar/index.jsx
@@ -0,0 +1,9 @@
+export default function RightBar() {
+     return(
+
+        <aside className="w-[350px] mr-2.5 " >
+            sag sidebar
+
+        </aside>
+     )
+}
---
+++ src/layouts/main/sidebar/menu/index.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import classNames from "classnames";
+import { mainMenu } from "~/utils/consts";
+
+
+export default function Menu() {
+  return (
+    <nav className="mt-0.5 mb-1">
+      {mainMenu.map((menu, index) => (
+        <NavLink key={index} to={menu.path} className="py-1 block group">
+          {({isActive}) => (
+            <div
+              className={classNames("p-3 rounded-full transition-colors inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]", {
+                "font-bold": isActive
+              })}>
+              <div className="w-[26.25px] h-[26.25] rel">
+                <span>{menu?.notification}</span>
+              {!isActive && menu.icon.passive}
+              {isActive && menu.icon.active}
+              </div>
+              <div className="pr-4 text-xl">
+                {menu.title}
+              </div>
+            </div>
+          )} 
+        </NavLink>
+      ))}
+    </nav>
+  )
+}
+
+
